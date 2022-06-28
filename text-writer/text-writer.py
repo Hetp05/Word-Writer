8284c49ad470e5930504eb82ec8099cad64c7462
Het05 <98163279+Hetp05@users.noreply.github.com>
2022-06-28 12:16:42 +0200

    visual bug fixes

--- text-writer/text-writer.py
+++ text-writer/text-writer.py
@@ -1,6 +1,11 @@
 # All the libraries # 
+from cgitb import text
+from contextlib import redirect_stdout
+from email.mime import image
+from logging import root
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.ttk import Style
 
 # Definitions #
 def open_file():
@@ -28,27 +33,48 @@ def save_file():
         output_file.write(text)
     window.title(f"Text Writer by Hetp - {filepath}")
 
-# Windows Sttings #
+def add_image():
+    global my_image
+    my_image = tk.PhotoImage(file="Reference/textedit-icon.png")
+    txt_edit.image_create(tk.END, image=my_image)
+
+# Windows Setings #
 window = tk.Tk()
-window.title("Text Editor by Buzz2Day Tech")
+window.title("Text Writer by Hetp ")
 
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Text(window)
-fr_button = tk.Frame(window, relief=tk.RAISED, bd=2)
+txt_edit = tk.Text(window, font=("Menlo"))
 
 
 # Buttons & Laylot #
-btn_open = tk.Button(fr_button, text="Open", command=open_file)
-btn_save = tk.Button(fr_button, text="Save As..", command=save_file)
-
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
-
-fr_button.grid(row=0,column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
 
+# Test #
+menubar = tk.Menu(window, foreground='black', activebackground='white', activeforeground='black')  
+file = tk.Menu(menubar, tearoff=0, foreground='black')  
+file.add_command(label="New")  
+file.add_command(label="Open", command=open_file)  
+file.add_command(label="Save")  
+file.add_command(label="Save as", command=save_file)
+file.add_command(label="Add Image", command=add_image,)    
+file.add_separator()  
+file.add_command(label="Exit", command=window.quit)  
+menubar.add_cascade(label="File", menu=file)  
 
-# Etc/ Other # 
+edit = tk.Menu(menubar, tearoff=0)  
+edit.add_command(label="Undo")  
+edit.add_separator()     
+edit.add_command(label="Cut")  
+edit.add_command(label="Copy")  
+edit.add_command(label="Paste")  
+menubar.add_cascade(label="Edit", menu=edit)  
+
+help = tk.Menu(menubar, tearoff=0)  
+help.add_command(label="About")  
+menubar.add_cascade(label="Help", menu=help)
+
+# Etc/ Other #
+window.config(menu=menubar)
 window.mainloop()
